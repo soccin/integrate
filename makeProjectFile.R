e024e71830a9cbf2082659ddcf6c2e3a0c879932
Nicholas D. Socci <soccin@mskcc.org>
2024-11-14 15:23:55 -0500

    Update makeProject script

--- makeProjectFile.R
+++ makeProjectFile.R
@@ -1,37 +1,49 @@
 usage<-function(){
-    cat("\n\tusage:makeProjectFile.R starManifest.csv tempoManifest.tsv tempoPairing.txt\n\n")
+    cat("\n\tusage:makeProjectFile.R tempoManifest.tsv tempoPairing.txt\n\n")
     quit()
 }
 
 argv=commandArgs(trailing=T)
 
-if(len(argv)!=3) {
+if(len(argv)!=2) {
     usage()
 }
 
 require(tidyverse)
 
-rna=read_csv(argv[1],col_names=c("SAMPLE","RBAM"))
-dna=read_tsv(argv[2])
-pairs=read_tsv(argv[3])
+# _Chimeric.out.srt.bam
+
+rnaBams=fs::dir_ls(recur=T,regex="_Chimeric.out.srt.bam")
+rna=tibble(
+    SAMPLE=basename(rnaBams) %>% gsub("__Chimeric.out.srt.bam$","",.),
+    RBAM=rnaBams
+)
+
+dna=read_tsv(argv[1])
+pairs=read_tsv(argv[2])
 
 tbams=dna %>%
     filter(SAMPLE %in% pairs$TUMOR_ID) %>%
     select(SAMPLE,TBAM=BAM) %>%
     mutate(SAMPLE=gsub("C-......_","",SAMPLE))
 
+tumorMarker=unique(stringr::str_extract(tbams$SAMPLE,"..$"))
+if(len(tumorMarker)!=1) {
+    cat("\n\n   Ambiguous tumorMarker [",tumorMarker,"]\n\n")
+    rlang::abort("FATAL::ERROR")
+}
+
 nbams=dna %>%
     filter(SAMPLE %in% pairs$NORMAL_ID) %>%
     select(SAMPLE,NBAM=BAM) %>%
     mutate(SAMPLE=gsub("C-......_","",SAMPLE)) %>%
-    mutate(SAMPLE=gsub("-N$","-T",SAMPLE))
+    mutate(SAMPLE=gsub(".N$",tumorMarker,SAMPLE))
 
 manifest=rna %>%
-    mutate(SAMPLE=paste0(SAMPLE,"-T")) %>%
+    mutate(SAMPLE=paste0(SAMPLE,tumorMarker)) %>%
     left_join(tbams) %>%
     filter(!is.na(TBAM)) %>%
     left_join(nbams) %>%
     filter(!is.na(NBAM))
 
 write_tsv(manifest,"_integrate_manifest.txt",col_names=F)
-
