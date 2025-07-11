e5a26e244b3f2a2ebf4ad5b4c5f319c2e65bff25
Nicholas D. Socci <soccin@mskcc.org>
2025-07-11 14:27:03 -0400

    feat: create makeIntegrateProjectFile script
    Add a new R script that generates an integrated manifest
    file from a headerless CSV input. The script requires a
    specific format for the input file and utilizes the
    tidyverse package for data manipulation. This addition
    enhances the project's capabilities for handling
    bioinformatics data integration. #cursor

---
+++ makeIntegrateProjectFile.R
@@ -0,0 +1,25 @@
+usage<-function(){
+    cat("
+  usage:makeIntegrateProjectFile.R bamManifest.csv
+    - bamManifest.csv headerless csv with the following columns
+      PATH,SID,TYPE
+      TYPE: T=>tumor
+            N=>normal
+            R=>RNA
+\n")
+    quit()
+}
+
+argv=commandArgs(trailing=T)
+
+if(len(argv)!=1) {
+    usage()
+}
+
+require(tidyverse)
+
+argv=commandArgs(trailing=T)
+mm=read_csv(argv[1],col_names=c("PATH","SID","TYPE"))
+
+mm %>% spread(TYPE,PATH) %>% write_tsv("_integrate_manifest.tsv",col_names=F)
+
